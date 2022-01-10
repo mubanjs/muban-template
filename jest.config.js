d9b1388760521eaffb688599bf4b3797bc7ca4f7
Arjan van Wijk <thanarie@gmail.com>
2022-01-10 18:37:27 +0100

    Update repo
    - switch from yarn to npm
    - remove unused dependencies and files/folders
    - switch to muban eslint config
    - add github actions

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  preset: 'ts-jest/presets/js-with-babel',
-  // testEnvironment: 'jsdom',
-  transformIgnorePatterns: ['<rootDir>/node_modules/(?!lodash-es)'],
-  setupFilesAfterEnv: ['<rootDir>/test-utils/setupTests.js'],
+  preset: 'ts-jest',
 };
